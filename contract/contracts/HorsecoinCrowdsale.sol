521d7093e5882e02dd0b9a1ffe1ca0b44873496c
Nick <nick@ab-it.io>
2018-02-01 03:17:39 +0200

    implemented bonus system

--- contract/contracts/HorsecoinCrowdsale.sol
+++ contract/contracts/HorsecoinCrowdsale.sol
@@ -14,30 +14,33 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
 
     address wallet = 0xb8D2Adb361e3C631951b636Dd6f68C7182d2E6c6;
+    address remainingTokensWallet = 0xb8D2Adb361e3C631951b636Dd6f68C7182d2E6c6;
 
     // Amount raised in PreICO
     // -------------------------
     uint256 public totalWeiInPreICO;
     // -----------------------
     uint256 private bonus;
+    uint256 private tokensCap;
+    bool isFinalized;
 
     // Token Distribution
     // -------------------
-    uint256 public maxTokens = 1000000000000 * power(10,18); // There will be total 1000000000000 HRC Tokens
-    uint256 public tokensForEcosystem = 200000000000 * power(10,18);
-    uint256 public tokensForTeam = 200000000000* power(10,18);
-    uint256 public tokensForBounty = 100000000000* power(10,18);
-    uint256 public totalTokensForSale = 600000000000 * power(10,18); // 600000000000 HRCs will be sold in Crowdsale
-    uint256 public totalTokensForSaleDuringPreICO = 100000000000 * power(10,18); // 100000000000 out of 600000000000 HRC will be sold during PreICO
+    uint256 public tokensForEcosystem = 100000000 * power(10, 18); // There will be total 100.000.000 HRC Tokens fot the ecosystem
+    uint256 public tokensForTeam = 100000000 * power(10, 18);// There will be total 100.000.000 HRC Tokens for the team
+    uint256 public tokensForBounty = 100000000 * power(10, 18); // There will be total 100.000.000 HRC Tokens for bounties
+    uint256 public totalTokensForSale = 100000000 * power(10, 18); // 100.000.000 HRCs will be sold in Crowdsale
+    uint256 public totalTokensForSaleDuringPreICO = 10000000 * power(10, 18); // 10.000.000 out of 500.000.000 HRC will be sold during PreICO
     // ==============================
 
     function power(uint256 A, uint256 B) public returns (uint256){
-        return A**B;
+        return A ** B;
     }
 
     // Events
     event EthTransferred(string text);
     event EthRefunded(string text);
+    event TokenMint(address indexed beneficiary, uint256 amount);
 
 
     function HorseCoinCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, uint256 _goal, address _wallet) public
@@ -46,6 +49,13 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     RefundableCrowdsale(_goal)
     Crowdsale(_startTime, _endTime, _rate, _wallet) {
         require(_goal <= _cap);
+        remainingTokensWallet = _wallet;
+        wallet = _wallet;
+        tokensCap = _cap;
+        // allocate tokens to Owners
+        mintTokens(_wallet, tokensForTeam);
+        mintTokens(_wallet, tokensForEcosystem);
+        mintTokens(_wallet, tokensForBounty);
     }
 
     // HRC Crowdsale Stages
@@ -53,6 +63,7 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
     // Change Crowdsale Stage. Available Options: PreICO, ICOWave1
 
+    //TODO: Automate switch of stage
     function setCrowdsaleStage(uint value) public onlyOwner {
         CrowdsaleStage _stage;
 
@@ -64,7 +75,7 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
         stage = _stage;
 
-        if (stage == CrowdsaleStage.PreICO)   {setCurrentBonus(200);}
+        if (stage == CrowdsaleStage.PreICO) {setCurrentBonus(200);}
         if (stage == CrowdsaleStage.ICOWave1) {setCurrentBonus(100);}
         if (stage == CrowdsaleStage.ICOWave2) {setCurrentBonus(75);}
         if (stage == CrowdsaleStage.ICOWave3) {setCurrentBonus(50);}
@@ -92,8 +103,9 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     // Override this method to have a way to add business logic to your crowdsale when buying
     // Returns weiAmount times rate by default
     function getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
-        return super.getTokenAmount((weiAmount) + (weiAmount * bonus)); //give them the bonus
-        }
+        return super.getTokenAmount((weiAmount) + (weiAmount * bonus));
+        //give them the bonus
+    }
 
     // Override to create custom fund forwarding mechanisms
     // Forwards funds to the specified wallet by default
@@ -117,6 +129,25 @@ contract HorseCoinCrowdsale is CappedCrowdsale, RefundableCrowdsale {
     // Override to execute any logic once the crowdsale finalizes
     // Requires a call to the public finalize method, only after the sale hasEnded
     function finalization() internal {
+        //mint tokens up to total cap
+         if (token.totalSupply() < tokensCap) {
+          mintTokens(remainingTokensWallet, tokensCap.sub(token.totalSupply()));
+         }
+        //no more tokens from now on
+         token.finishMinting();
+        isFinalized = true;
         return super.finalization();
+    }
+
+
+    function mintTokens(address beneficiary, uint256 tokens) public onlyOwner {
+        require(beneficiary != 0x0);
+
+        // Cannot mint after sale is closed
+        require(!isFinalized);
+
+
+        token.mint(beneficiary, tokens);
+        TokenMint(beneficiary, tokens);
     }
 }
